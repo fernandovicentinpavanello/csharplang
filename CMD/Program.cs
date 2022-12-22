efa5d4592950f54d57640cde4bc7afea6dfc3900
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2022-12-22 18:12:41 -0300

    Second project done. CMD - Add users, remove, see the list

--- CMD/Program.cs
+++ CMD/Program.cs
@@ -1,2 +1,158 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CMD
+{
+    class Program
+    {
+
+        [System.Serializable]
+        struct Cliente
+        {
+            public string nome;
+            public string email;
+            public string cpf;
+        }
+
+        static List<Cliente> clientes = new List<Cliente>();
+
+        enum Menu { Listagem = 1, Adicionar, Remover, Sair }
+
+
+        static void Main(string[] args)
+        {
+
+            Carregar();
+            bool escolherSair = false;
+            while (!escolherSair)
+            {
+                Console.WriteLine("Sistema de clientes - Bem vindo!");
+                Console.WriteLine("1-Listagem\n2-Adicionar\n3-Remover\n4-Sair");
+                int intOp = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intOp;
+
+                switch (opcao)
+                {
+                    case Menu.Listagem:
+                        Listagem();
+                        break;
+                    case Menu.Adicionar:
+                        Adicionar();
+                        break;
+                    case Menu.Remover:
+                        Remover();
+                        break;
+                    case Menu.Sair:
+                        escolherSair = true;
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+
+        static void Listagem()
+        {
+
+            if (clientes.Count > 0)
+            {
+            
+                Console.WriteLine("Lista de clientes: ");
+                int i = 0;
+                foreach (Cliente cliente in clientes)
+                {
+                    Console.WriteLine($"ID: {i}");
+                    Console.WriteLine($"Nome: {cliente.nome}");
+                    Console.WriteLine($"E-mail: {cliente.email}");
+                    Console.WriteLine($"CPF: {cliente.cpf}");
+                    Console.WriteLine($"--------------------");
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum cliente cadastrado!");
+            }
+
+
+            Console.WriteLine("Aperte enter para sair.");
+            Console.ReadLine();
+        }
+
+
+        static void Adicionar()
+        {
+            Cliente cliente = new Cliente();
+            Console.WriteLine("Cadastro de cliente: ");
+            Console.WriteLine("Nome do cliente: ");
+            cliente.nome = Console.ReadLine();
+            Console.WriteLine("Email do cliente: ");
+            cliente.email = Console.ReadLine();
+            Console.WriteLine("CPF do cliente: ");
+            cliente.cpf = Console.ReadLine();
+
+            clientes.Add(cliente);
+            Salvar();
+
+            Console.WriteLine("Cadastro concluído! Aperte enter para sair.");
+            Console.ReadLine();
+        }
+
+
+        static void Remover()
+        {
+
+            Listagem();
+            Console.WriteLine("Digite o ID do cliente que você quer remover:");
+            int id = int.Parse(Console.ReadLine());
+            if(id >= 0 && id < clientes.Count)
+            {
+                clientes.RemoveAt(id);
+                Salvar();
+
+            }
+            else
+            {
+                Console.WriteLine("Id digitando é invalido, tente novamente!");
+                Console.ReadLine();
+            }
+        }        
+
+
+        static void Salvar()
+        {
+            FileStream stream = new FileStream("clients.dat", FileMode.OpenOrCreate);
+            BinaryFormatter enconder = new BinaryFormatter();
+
+            enconder.Serialize(stream, clientes);
+
+            stream.Close();
+
+        }
+
+
+        static void Carregar()
+        {
+
+            FileStream stream = new FileStream("clients.dat", FileMode.OpenOrCreate);
+
+
+            try
+            {
+                BinaryFormatter enconder = new BinaryFormatter();
+
+                clientes = (List<Cliente>)enconder.Deserialize(stream);
+
+                if(clientes == null)
+                {
+                    clientes = new List<Cliente>();
+                }
+            }
+            catch (Exception e)
+            {
+                clientes = new List<Cliente>();                
+            }
+
+            stream.Close();        
+        }
+
+    }
+}
