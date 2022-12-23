953041c1990a292816dcf558dcbd0da920f77119
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2022-12-23 20:57:00 -0300

    Examples for POO ith C#.

---
+++ POO/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace POO 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Filme filme = new Filme();
+            filme.nome = "Scarface";
+            filme.Executar();
+
+            Console.ReadLine();
+        }
+    } 
+
+    class Filme
+    {
+        public string nome;
+        public string desc;
+        public int ano;
+        public string studio;
+        public List<string> atores = new List<string>();
+
+
+        public void Executar()
+        {
+            Console.WriteLine("Rodando filme: " + nome);
+        }
+
+        public void Pausar()
+        {
+            Console.WriteLine("||");
+        }
+    }
+
+}
