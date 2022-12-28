e7c524f0a2fc4efe1cf986c21290dc0f89084483
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2022-12-28 10:34:23 -0300

    Doing it again. Install a new extansion at VC code to try again without errors.

--- POO/Program.cs
+++ POO/Program.cs
@@ -5,32 +5,9 @@ namespace POO
     {
         static void Main(string[] args) 
         {
-            Filme filme = new Filme();
-            filme.nome = "Scarface";
-            filme.Executar();
-
-            Console.ReadLine();
+        
         }
+            
     } 
-
-    class Filme
-    {
-        public string nome;
-        public string desc;
-        public int ano;
-        public string studio;
-        public List<string> atores = new List<string>();
-
-
-        public void Executar()
-        {
-            Console.WriteLine("Rodando filme: " + nome);
-        }
-
-        public void Pausar()
-        {
-            Console.WriteLine("||");
-        }
-    }
 
 }
