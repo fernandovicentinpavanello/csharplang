a832b042c6b494e9245995f9366787ac93e4c34d
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2022-12-28 14:26:59 -0300

    Start the third project - Gestor de Estoque orientado a objetos no CMD

---
+++ Gestor/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
