2bcc9ac4f30796430fc0e73a14a78dc6cc0f0f1a
Fernando Vicentin Pavanello <vicentin@tutanota.com>
2023-01-03 19:03:20 -0300

    Finished the project - HTTP with C# and JSON

--- HTTP/Program.cs
+++ HTTP/Program.cs
@@ -2,12 +2,22 @@ using System;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace HTTP
 {
     public class Program
     {
         static void Main(string[] args)
+        {
+            ReqList();
+            ReqUnica();
+            Console.ReadLine();
+
+        }
+
+        static void ReqList()
         {
             var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
             requisicao.Method = "GET";
@@ -19,13 +29,39 @@ namespace HTTP
                 StreamReader leitor = new StreamReader(stream);
                 object dados = leitor.ReadToEnd();
 
-                Console.WriteLine(dados.ToString());
+                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                foreach (Tarefa tarefa in tarefas)
+                {
+                    tarefa.Exibir();
+                }
 
                 stream.Close();
                 resposta.Close();
             }
-
-
         }
+
+        static void ReqUnica()
+        {
+            String id = Console.ReadLine();
+            var requisicao = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/"+id);
+            requisicao.Method = "GET";
+            var resposta = requisicao.GetResponse();
+
+            using (resposta)
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                Tarefa tarefa = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+
+                tarefa.Exibir();
+
+                stream.Close();
+                resposta.Close();
+            }
+        }
+
     }
 }
---
+++ HTTP/Tarefa.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    public class Tarefa
+    {
+        public int userId;
+        public int id;
+        public string? title;
+        public bool completed;
+
+        public void Exibir()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Objeto Tarefa");
+            Console.WriteLine($"User id: {userId}");
+            Console.WriteLine($"id: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Finalizou: {completed}");
+            Console.WriteLine("");
+            Console.WriteLine("==============================");
+
+        }
+    }
+}
